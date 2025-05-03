1c10a9d8b0189be304a36333bdb0988cd201329c
Aibar00 <aibarotar123@gmail.com>
2025-05-03 15:04:46 +0500

    same merge

--- auth.py
+++ auth.py
@@ -112,7 +112,7 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        payload = jwt.decode(token.credentials, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
